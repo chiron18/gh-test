37782c1cab17282f26fbd388ec7cfce12960079a
chiron18 <41318970+chiron18@users.noreply.github.com>
2023-08-22 21:55:00 -0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,9 @@ from st_files_connection import FilesConnection
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.experimental_connection('gcs', type=FilesConnection)
-df = conn.read("streamlit-bucket/myfile.csv", input_format="csv", ttl=600)
+df = conn.read("anzac723/2023_expenses.csv", input_format="csv", ttl=600)
 
 # Print results.
-for row in df.itertuples():
-    st.write(f"{row.Owner} has a :{row.Pet}:")
+st.dataframe(df)
 
 
